10d8352cf57ac9360186dcae3f1002f352310c80
Raafa1993 <rafaelajds@gmail.com>
2020-06-18 13:07:08 -0300

    add arq json para consumir internamente

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initAnimaNumeros from './modules/anima-numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
 
 
 initScrollSuave();
@@ -20,3 +21,4 @@ initDropdownMenu();
 initMenuMobile();
 initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
